3d19663724332c71b965b1b0d741bd46802c5c88
Josh Mahony <josh.mahony@atomsupplies.com>
2025-03-27 11:30:59 +0000

    add count

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 export const experimental_ppr = true
 
+let myCount = 1;
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  console.log('layout running');
+  console.log('layout running', myCount);
 
   return (
     <html lang="en">
       <body>
+        {`count${myCount++}`}
         {children}
       </body>
     </html>
